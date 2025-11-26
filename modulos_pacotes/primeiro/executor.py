0919fffcd9eb91fa1e155681827859a28f1cf0a8
unknown <josukeparte8@gmail.com>
2025-11-26 18:42:50 -0300

    Algo interessante

--- modulos_pacotes/primeiro/executor.py
+++ modulos_pacotes/primeiro/executor.py
@@ -1,8 +1,4 @@
-from meu_modulo import moeda, dobro, metade, mais_porc, menor_porc 
-valor = int(input('Digite o valor: '))
+from dindim import meu_modulo
+valor = int(input('Digite um valor: '))
 taxa = int(input('Qual o valor da taxa (Digite zero(0) para anular)? '))
-print(f'A metade de {moeda(valor)} é igual a {moeda(metade(valor))}')
-print(f'O dobro de {moeda(valor)} é {moeda(dobro(valor))}')
-if taxa != 0: 
-    print(f'R${moeda(valor)} aumentado em {taxa}% é R${moeda(mais_porc(valor,taxa))}')
-    print(f'R${moeda(valor)} diminuido em {taxa}% é R${moeda(menor_porc(valor,taxa))}')
+meu_modulo.reduzidor(valor, taxa)
---
+++ modulos_pacotes/primeiro/teste.py
@@ -0,0 +1,4 @@
+from dindim import meu_modulo
+
+val = float(input('Digite um valor: '))
+meu_modulo.reduzidor(val)
